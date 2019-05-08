a73b559b3702da089bbb47dc60b8768a4cd8bb65
mdayunus <mdayunus@gmail.com>
2019-05-08 19:17:19 +0530

    get data from url in json form

---
+++ News Headyng/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  News Headyng
+//
+//  Created by Mohammad Yunus on 08/05/19.
+//  Copyright © 2019 taptap. All rights reserved.
+//
+
+import Foundation
+struct Constants {
+    static var cellID = "cell"
+    static var key = "85l01ds36GzAf05TYoPkjATZx76UuuU8"
+    static var newsURL = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key="
+}
--- News Headyng/HomeTableViewController.swift
+++ News Headyng/HomeTableViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class HomeTableViewController: UITableViewController {
     
-    let cellID = "cell"
+    let decoder = JSONDecoder()
+    
+    let cache = NSCache<NSString, NSData>()
+    
+    var allNews: Body?
+    
     @IBOutlet weak var refreshControlOutlet: UIRefreshControl!
     
     @IBAction func refreshControlAction(_ sender: UIRefreshControl) {
@@ -20,16 +25,48 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func getAllNewsFrom(url: String){
+        if let youareL = URL(string: url){
+            let task = URLSession.shared.dataTask(with: youareL) { [weak self] (data, response, error) in
+                if error != nil{
+                    print(error!)
+                    //
+                }else{
+                    if let d = data{
+                        do{
+                            self?.allNews = try self?.decoder.decode(Body.self, from: d)
+                            DispatchQueue.main.async {
+                                self?.tableView.reloadData()
+                                self?.refreshControlOutlet.endRefreshing()
+                            }
+                        }catch{
+                            print(error)
+                            //
+                        }
+                    }else{
+                        print("d is not equal to data")
+                        //
+                    }
+                }
+            }
+            task.resume()
+        }else{
+            print("string provided can not be converted to URL")
+            //
+        }
+        
+    }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return allNews?.num_results ?? 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellID, for: indexPath)
 
         // Configure the cell...
 
